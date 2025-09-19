1018d91781e3bb3e88cb3664c0622fe76e40a658
nomjie33 <naomi011806@gmail.com>
2025-09-19 23:16:41 +0800

    create register account function

---
+++ MCO1/JSver/modules/accountManager.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+var accounts = [];
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function registerAccount(){
+    console.log("Register Account Name");
+    rl.question("Account Name: ", function(accountName) {
+        const newAccount = { 
+            name: accountName, balance: 0
+        };
+        accounts.push(newAccount);
+        console.log(`Account "${accountName}" registered successfully.`);
+        rl.close(); // Close the readline interface
+    });
+}
+
