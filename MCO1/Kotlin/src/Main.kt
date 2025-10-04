a7f59730e8bc025875c6aeaeb61cc6376878b4ea
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-10-04 14:32:51 +0800

    added main menu display

--- MCO1/Kotlin/src/Main.kt
+++ MCO1/Kotlin/src/Main.kt
@@ -1,5 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun mainMenu(): Int {
+    print("Select Transaction: \n" +
+            "[1] Register Account Name \n" +
+            "[2] Deposit Amount \n" +
+            "[3] Withdraw Amount \n" +
+            "[4] Currency Exchange \n" +
+            "[5] Record Exchange Rates \n" +
+            "[6] Show Interest Computation \n" +
+            "[7] Leave \n")
+    return readln().toInt()
+}
 fun main() {
-    println("Hello World")
+//    println("Hello World")
+    var choice = mainMenu()
+
 }
