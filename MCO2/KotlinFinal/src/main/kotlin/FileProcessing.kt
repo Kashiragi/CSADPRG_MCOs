cd1c514db8b0f110f9f78e9b2e5adc3436214766
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-10 18:31:34 +0800

    add: Added file processing functions for reading csv file

---
+++ MCO2/KotlinFinal/src/main/kotlin/FileProcessing.kt
@@ -0,0 +1,93 @@
+package org.mco2
+
+import java.io.File
+import org.jetbrains.kotlinx.dataframe.*
+import org.jetbrains.kotlinx.dataframe.api.*
+import org.jetbrains.kotlinx.dataframe.io.*
+import java.time.temporal.ChronoUnit
+import java.time.LocalDate
+
+interface Schema{
+    val MainIsland : String?
+    val Region : String?
+    val Province : String?
+    val LegislativeDistrict : String?
+    val Municipality : String?
+    val DistrictEngineeringOffice : String?
+    val ProjectId : String?
+    val ProjectName : String?
+    val TypeOfWork : String?
+    val FundingYear: Int?
+    val ContractId : String?
+    val ApprovedBudgetForContract: Double?
+    val ContractCost: Double?
+    val ActualCompletionDate: LocalDate?
+    val Contractor : String?
+    val ContractorCount: Int?
+    val StartDate: LocalDate?
+    val ProjectLatitude:Double?
+    val ProjectLongitude: Double?
+    val ProvincialCapital : String?
+    val ProvincialCapitalLatitude: Double?
+    val ProvincialCapitalLongitude: Double?
+}
+fun ReadRawFile(path: String): DataFrame{
+    val raw = DataFrame
+        .readCsv(
+            File(path)
+        )
+    val ogCount = raw.count()
+    val conv = raw
+        .convert("FundingYear","ContractorCount").with{ it -> it.toString().toIntOrNull() }
+        .convert("ApprovedBudgetForContract", "ContractCost", "ProjectLatitude", "ProjectLongitude", "ProvincialCapitalLatitude", "ProvincialCapitalLongitude").with{ it.toString().toDoubleOrNull() }
+    val df = conv
+        .convertTo<Schema>()
+    //df.schema().print()
+    print("${df.count()} rows loaded")
+    return df
+}
+fun CleanDataFrame(df: DataFrame): DataFrame{
+    // exclude invalid values
+    //val columns = listOf("MainIsland","Region","Province","LegislativeDistrict","Municipality","DistrictEngineeringOffice","ProjectId","ProjectName","TypeOfWork","FundingYear","ContractId","ApprovedBudgetForContract","ContractCost","ActualCompletionDate","Contractor","ContractorCount","StartDate","ProjectLatitude","ProjectLongitude","ProvincialCapital","ProvincialCapitalLatitude","ProvincialCapitalLongitude")
+    //
+    //val cleanDf = df.filter { row ->
+    //    columns.all{ col -> row[col] != null }
+    //}
+
+    // Remove all rows with empty values in at least 1 column
+    // this makes life simpler oop-
+    val cleanDf = df.dropNulls()
+
+    val cleanCount= cleanDf.count()
+    println("original count: ${df.count()}")
+    println("new Count: $cleanCount")
+
+    val validYearsDf = cleanDf
+        .filter { row ->
+        val year = row["FundingYear"] as Int
+
+        year!=null && year in 2021..2023
+    }
+    val validYearsCount = validYearsDf.count()
+    println(validYearsCount)
+
+    print("${cleanDf.count()} filtered for 2021-2023")
+    return cleanDf
+}
+
+fun ComputeNewColumns(df: DataFrame): DataFrame{
+
+    val withNewCols = df
+        .add("CostSavings") { row ->
+            val budget = row["ApprovedBudgetForContract"] as? Double ?: 0.0
+            val cost = row["ContractCost"] as? Double ?: 0.0
+            budget - cost
+        }
+        .add("CompletionDelayDays") {row ->
+            val start: LocalDate = row["StartDate"] as? LocalDate
+            val end = row["ActualCompletionDate"] as? LocalDate
+            if(start!=null && end!=null) ChronoUnit.DAYS.between(start, end)
+            else 0
+        }
+    return withNewCols
+}
